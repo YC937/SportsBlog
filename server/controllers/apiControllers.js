772bf964a903fd1e5acbb3acddd0c7bc868b0607
Garrett Moody <garrettmoody011@gmail.com>
2023-08-28 21:04:22 -0400

    added a routes file with basic api.js code and added a controllers file with basic apiControllers.js code

---
+++ server/controllers/apiControllers.js
@@ -0,0 +1,11 @@
+const ApiController = {
+    getSportsData(req, res) {
+      
+      const sportsData = [{ /* ... */ }];
+  
+      res.json(sportsData);
+    },
+  };
+  
+  module.exports = ApiController;
+  
