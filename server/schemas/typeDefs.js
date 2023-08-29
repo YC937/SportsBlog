a028795df579335e422f924d285cbae56619a911
mserafin20 <mmserafin20@gmail.com>
2023-08-29 16:18:12 -0400

    added in the typedefs for user and the auth

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,5 +1,19 @@
 const typeDefs = `
+type User {
+    _id: ID!
+    name: String!
+    email: String!
+}
 
+type Auth {
+    token: ID!
+    user: User
+}
+
+type Mutation {
+    login(email: String!, password: String!): Auth
+    signup(name: String!, email: String!, password: String!): Auth
+}
 `;
 
 module.exports = typeDefs;
