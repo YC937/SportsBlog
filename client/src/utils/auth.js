e4332b7ebbb75d69eb8181396668ccfb7f3ea1bc
mserafin20 <mmserafin20@gmail.com>
2023-08-31 17:03:41 -0400

    added to App, created auth file for client side

---
+++ client/src/utils/auth.js
@@ -0,0 +1,7 @@
+const tokenName = 'user_token';
+
+export default {
+    getToken: () => localStorage.getItem(tokenName),
+    setToken: (token) => localStorage.setItem(tokenName, token),
+    deleteToken: () => localStorage.removeItem(tokenName)
+}
