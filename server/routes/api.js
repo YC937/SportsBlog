473bd57c59765765d5aa6ebe3a0f457bc2cfc2d4
lo-pezzy <lopezchr817@gmail.com>
2023-08-31 18:38:18 -0400

    adding env file and openweather api key

--- server/routes/api.js
+++ server/routes/api.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const ApiController = require('../controllers/apiController');
 
+
 router.get('/sports', ApiController.getSportsData);
 
 module.exports = router;
