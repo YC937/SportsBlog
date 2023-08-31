6793ad73ffe2c53aacb27f3a70cc305994ef57cb
mserafin20 <mmserafin20@gmail.com>
2023-08-31 13:18:20 -0400

    updated User.js, added hooks for testing

--- server/models/User.js
+++ server/models/User.js
@@ -1,5 +1,13 @@
-const { Schema, model } = require('mongoose');
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
 const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+const validateEmail = function (email) {
+    const regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+    return regex.test(email);
+};
 
 const userSchema = new Schema({
     name: {
@@ -11,11 +19,7 @@ const userSchema = new Schema({
         type: String,
         required: true,
         unique: true,
-        validate: {
-            validator: function (email) {
-                return /.+@.+\..+/.test(email);
-            },
-        },
+        validate: [validateEmail, 'Please enter a valid email address'],
     },
     password: {
         type: String,
@@ -24,21 +28,42 @@ const userSchema = new Schema({
     },
 });
 
-// set up pre-save middleware to create password
-userSchema.pre('save', async function (next) {
-    if (this.isNew || this.isModified('password')) {
-      const saltRounds = 10;
-      this.password = await bcrypt.hash(this.password, saltRounds);
+// Pre-hook functions that can be exported for testing
+const hashPassword = async function (next) {
+    if(this.isNew || this.isModified('password')) {
+        this.password = await bcrypt.hash(this.password, saltRounds);
     }
-  
+
     next();
-  });
+};
+
+// Pre-hook functions that can be exported for testing
+const hashAllPasswords = async function (next, docs) {
+    if(Array.isArray(docs) && docs.length) {
+        const hashedUsers = docs.map( async (user) => {
+            user.password = await bcrypt.hash(user.password, saltRounds);
+            return user;
+        });
+        const users = await Promise.all(hashedUsers);
+        next();
+    }
+    else {
+        return next(new Error('User list should not be empty'));
+    }
+
+    next();
+}
+
+// set up pre-save middleware to create password
+userSchema.pre('save', hashPassword);
+userSchema.pre('insertMany', hashAllPasswords);
+    
 
   // compare the incoming password with the hashed password
 userSchema.methods.isCorrectPassword = async function (password) {
-    return bcrypt.compare(password, this.password);
+    return await bcrypt.compare(password, this.password);
   };
 
-const User = model('User', userSchema);
+const User = mongoose.model('User', userSchema);
 
 module.exports = User;
