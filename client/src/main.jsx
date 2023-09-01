a50dd12a4cc928a801a97e49ffdfb04c618c2767
mserafin20 <mmserafin20@gmail.com>
2023-08-31 21:03:36 -0400

    edit to main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,40 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: '/login',
+        element: <Login />,
+      },
+      {
+        path: '/logout',
+        element: <Logout />,
+      },
+      // {
+      //   path: '/stadiums_and_upcoming-games',
+      //   element: <Home />,
+      // },
+      // {
+      //   path: '/search',
+      //   element: <Home />,
+      // },
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router} />
 )
