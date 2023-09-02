c77b14883aa5f15d75bbdd520659f5cb02df281b
mserafin20 <mmserafin20@gmail.com>
2023-09-02 15:12:21 -0400

    added SignUpForm and made other edits

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,14 +1,26 @@
 import { gql } from '@apollo/client';
 
-export const MUTATION_LOGIN = gql`
-mutation Mutation($email: String!, $password: String!) {
-    login(email: $email, password: $password) {
-      token
-      user {
-        _id
-        email
-        name
-      }
+export const MUTATION_SIGNUP = gql`
+mutation Mutation($username: String!, $email: String!, $password: String!) {
+  signup(username: $username, email: $email, password: $password) {
+    user {
+      _id
+      username
+      email
     }
   }
+}
+`
+
+export const MUTATION_LOGIN = gql`
+mutation Mutation($email: String!, $password: String!) {
+  login(email: $email, password: $password) {
+    token
+    user {
+      _id
+      username
+      email
+    }
+  }
+}
 `
