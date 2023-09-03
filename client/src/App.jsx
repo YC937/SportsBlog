eca7a31858569f9099a6d03c13380ddf9fa09c6a
mserafin20 <mmserafin20@gmail.com>
2023-09-03 12:17:23 -0400

    debugging

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,6 +14,7 @@ import { Outlet } from 'react-router-dom';
 import { setContext } from '@apollo/client/link/context';
 
 import Auth from './utils/auth';
+import Nav from './components/Nav';
 
 
 // Main GraphQL endpoint
